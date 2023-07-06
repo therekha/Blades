7c9c5d8e9111ddc21141e4b75183e02139a9f450
Rekha Bhupatiraju <therekha@gmail.com>
2023-07-06 19:56:13 -0400

    i have combined the bots. aha

--- embed.js
+++ embed.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js");
 const importObj = {
 	...require("./embeds/character/actions.js"),
 	...require("./embeds/character/charitems.js"),
@@ -24,8 +25,12 @@ for (let prop in importObj) {
 	module.exports[prop] = importObj[prop];
 }
 
-module.exports.help = `**Help**
-Note: Some of these nested commands.
+
+module.exports.help = new Discord.MessageEmbed()
+	.setColor("#0099ff")
+	.setTitle("Help")
+	.setDescription(
+		`### GLOSSARY HELP
 
 **Procedures**
 \` $engagement \` Describes the Engagement roll process.
@@ -51,4 +56,224 @@ Special playbook items can be found by the name of the playbook: \` $cutter \`, 
 \` $names \` For example names.
 \` $districts \` Lists districts and their commands.
 \` $glossary \` Defines common terms.
-\` $heritages \` Gives commands for heritages.`;
+\` $heritages \` Gives commands for heritages.
+
+### DICE HELP`
+	)
+	.addFields(
+		{
+			name: `You say...`,
+			inline: true,
+			value: `\` !2 \`
+		\` !3r \`
+		\` !2r /comment \``,
+		},
+		{
+			name: `To...`,
+			inline: true,
+			value: `Roll 2 d6s.
+		Resistance roll of 3 d6s.
+		Add a comment.`,
+		},
+		{
+			name: `Show this message again:`,
+			value: `\` !help \` `,
+		},
+		{
+			name: `Basic mechanics`,
+			value: `\` !dice \`
+		\` !position \`
+		\` !effect \`
+		\` !group \``,
+			inline: true,
+		},
+		{
+			inline: true,
+			name: `to help out`,
+			value: `Lists how to get extra dice.
+		Explains position & effect.
+		Also works.
+		Explains Group Actions`,
+		},
+		{
+			name: `Devil's Bargain reference`,
+			value: `\` !bargain \` Give a couple suggestions for Devil's Bargains.`,
+		},
+		{
+			inline: true,
+			name: `Consequence reference`,
+			value: `\` !controlled \`
+		\` !risky \`
+		\` !desperate \``,
+		},
+		{
+			inline: true,
+			name: `Lists...`,
+			value: `Controlled consequences
+		Risky consequences
+		Desperate consequences`,
+		},
+		{
+			name: `Bot code credit!`,
+			value: `[Blades in the Glossary](https://github.com/jordanclarkedev/Blades-in-the-Glossary/blob/master/README.md), [Blades in the Dicebot](https://github.com/jordanclarkedev/bitdicebot/blob/master/README.md)`,
+		}
+	);
+
+module.exports.position = new Discord.MessageEmbed()
+	.setColor("#0099ff")
+	.setTitle("What's your Position & Effect?")
+	.setDescription(
+		`Position and Effect are important to discuss with your GM. 
+	They can be thought of as 'risk' versus 'reward'; they describe how rewarding success will be, and the consequences you're risking.`
+	)
+	.addFields(
+		{
+			name: `Position...`,
+			inline: true,
+			value: `Controlled
+		Risky
+		Desperate`,
+		},
+		{
+			name: `Effect...`,
+			inline: true,
+			value: `Great
+		Standard
+		Limited
+		`,
+		}
+	);
+
+module.exports.effect = module.exports.position;
+
+module.exports.dice = new Discord.MessageEmbed()
+	.setColor("#0099ff")
+	.setTitle("Want extra dice?")
+	.setDescription(
+		`You can gain extra dice for your roll. Push yourself, ask crewmembers for an assist, or the GM for a Devil's Bargain... Or you can ask me about Group Actions with !group`
+	)
+	.addFields(
+		{
+			name: `Gain dice by...`,
+			inline: true,
+			value: `Pushing Yourself
+		Assists
+		Devil's Bargain`,
+		},
+		{
+			name: `At the cost of...`,
+			inline: true,
+			value: `Take two stress
+		They take one stress
+		Ask your GM for details`,
+		}
+	);
+
+module.exports.group = new Discord.MessageEmbed()
+	.setColor("#0099ff")
+	.setTitle("Group Actions")
+	.setDescription(
+		`Teamwork makes the dreamwork! Pick one person to lead your group action. Anyone who wants to partake in the action can roll, and the highest result counts for the entire group! Anyone who fails (1-3) makes the leader suffer 1 stress`
+	)
+	.addFields({
+		name: `A word of caution...`,
+		value: `If the group action results in consequences, the entire group may suffer the consequences; an easy way to TPK, if you're not cautious.`,
+	});
+
+module.exports.controlled = new Discord.MessageEmbed()
+	.setColor("#0099ff")
+	.setTitle("Controlled Consequences")
+	.setDescription(
+		`Don't worry, you can always resist consequences to lower the severity of them!`
+	)
+	.addFields(
+		{
+			name: `Stop!`,
+			value: `Reduced Effect:
+		Worse Position:
+		Lost Opportunity:
+		Complication:
+		Harm:`,
+			inline: true,
+		},
+		{
+			name: `You violated the law!`,
+			value: `-1 effect level
+			-1 position (can try again if failure)
+			Try again with a new action rating.
+			Immediate problem, 1 tick or +1 Heat.
+			Lesser Harm (level 1)`,
+			inline: true,
+		}
+	);
+
+module.exports.risky = new Discord.MessageEmbed()
+	.setColor("#0099ff")
+	.setTitle("Risky Consequences")
+	.setDescription(
+		`Don't worry, you can always resist consequences to lower the severity of them!`
+	)
+	.addFields(
+		{
+			name: `Stop right there,`,
+			value: `Reduced Effect:
+		Worse Position:
+		Lost Opportunity:
+		Complication:
+		Harm:`,
+			inline: true,
+		},
+		{
+			name: `criminal scum!`,
+			value: `-1 effect level
+		-1 position (try again if fail)
+		Try again with a new action
+		Immediate problem, 2 ticks or +1 Heat
+		Moderate Harm (level 2)
+		`,
+			inline: true,
+		}
+	);
+
+module.exports.desperate = new Discord.MessageEmbed()
+	.setColor("#0099ff")
+	.setTitle("Desperate Consequences")
+	.setDescription(
+		`Don't worry, you can always resist consequences to lower the severity of them! Also, you get XP for Desperate actions!`
+	)
+	.addFields(
+		{
+			name: `Then pay`,
+			value: `Reduced Effect:
+		Worse Position:
+		Lost Opportunity:
+		Complication:
+		Harm:`,
+			inline: true,
+		},
+		{
+			name: `with your blood!`,
+			value: `-1 effect level
+		-1 position (try again if fail)
+		Try again with a new action
+		Severe problem, 3 ticks or +2 Heat
+		Severe Harm (level 3)
+		`,
+			inline: true,
+		}
+	);
+
+module.exports.bargain = new Discord.MessageEmbed()
+	.setColor("#0099ff")
+	.setTitle(`Devil's Bargains`)
+	.setDescription(`Can be proposed by GM or other player. +1d for accepting.`)
+	.addFields({
+		name: `Some suggested bargains`,
+		value: `- Collateral damage, unintended harm.
+	- Sacrifice COIN or an item.
+	- Betray a friend or loved one.
+	- Offend or anger a faction.
+	- Start and/or tick a troublesome clock.
+	- Add HEAT to the crew from evidence or witnesses.
+	- Suffer harm.`,
+	});
--- index.js
+++ index.js
@@ -10,6 +10,119 @@ bot.on("ready", () => {
 	console.log("This bot is online");
 });
 
+const roll = {
+	//Parsing the user's string. Obtaining dice number (max 20), resist bool, comment.
+	parse(content) {
+		const data = {};
+
+		data.statement = content;
+		
+		//Comment handing
+		data.comment = ""; //Avoids concat "undefined" with "\n I'm limited...."
+		if (content.includes("/")) {
+			data.comment = "\n/ " + content.slice(content.indexOf("/") + 1) || "";
+		}
+
+		//Calculates number of dice to roll.
+		data.d = Number(/^\d+/.exec(content)); //Number must immediately follow !, can be many digits.
+		if (data.d > 20) {
+			data.d = 20;
+			data.comment +=
+				"\nI'm limited to rolling 20 dice at a time. I hope you don't mind!";
+		}
+		data.dice = data.d || 2; //Handles 0d rolls.
+
+		//Resist checker
+		if (data.statement.toLowerCase().includes("r")) {
+			data.resist = true;
+		}
+
+		return this.roller(data);
+	},
+
+	//Generates random numbers
+	roller(data) {
+		//Rolling dice into data.rolls[]
+		data.rolls = [];
+		data.index = 0;
+		data.result = 0;
+
+		for (i = 1; i <= data.dice; i++) {
+			data.rolls.push(Math.floor(Math.random() * 6 + 1));
+		}
+		if (data.d === 0) {
+			return this.zeroHandle(data); //Roll 2d, take lowest
+		} else {
+			return this.manyHandle(data); //Take highest of data.rolls
+		}
+	},
+
+	//Handling 0d rolls (roll 2, take lowest)
+	zeroHandle(data) {
+		if (data.rolls[0] > data.rolls[1]) {
+			data.result = data.rolls[1];
+			data.rolls[1] = `**${data.result}**`;
+		} else {
+			data.result = data.rolls[0];
+			data.rolls[0] = `**${data.result}**`;
+		}
+
+		return this.commenter(data);
+	},
+
+	//Default roll handler
+	manyHandle(data) {
+		data.rolls.forEach((value, index) => {
+			if (value > data.result) {
+				//Stores highest roll + the index of it.
+				data.result = value;
+				data.index = index;
+			} else if (value === 6 && data.result === 6) {
+				//Bolds duplicate 6s if they exist (crit handling).
+				data.rolls[index] = "**6**";
+				data.crit = true;
+			}
+		});
+		data.rolls[data.index] = `**${data.result}**`; //Bolds the first occurence of highest roll.
+
+		return this.commenter(data);
+	},
+
+	//Formatting reply string.
+	commenter(data) {
+		let replyString = `[**${data.result}**] `;
+
+		if (data.d !== 1) {
+			replyString += `from ${data.rolls.join(", ")}`; //Doesn't bother with displaying roll array if 1d.
+		}
+
+		if (data.comment) {
+			replyString += data.comment;
+		}
+
+		if (data.resist) {
+			//Resistance rolls
+			if (data.crit) {
+				return `**Critical!** Recover 1 stress\n${replyString}`;
+			} else {
+				return `**Take ${6 - data.result} stress!**\n${replyString}`;
+			}
+		} else {
+			//Action rolls
+			switch (true) {
+				case data.crit:
+					return `**Critical!**\n${replyString}`;
+				case data.result === 6:
+					return `**Success!**\n${replyString}`;
+				case data.result >= 4:
+					return `**Partial!**\n${replyString}`;
+				case data.result <= 3:
+					return `**Failure!**\n${replyString}`;
+			}
+		}
+	},
+};
+
 bot.on("message", (msg) => {
 	if (msg.content[0] === "$") {
 		let content = msg.content.slice(1).toLowerCase().replace(/\s+/g, "");
@@ -17,6 +130,19 @@ bot.on("message", (msg) => {
 		if (obj[content]) {
 			msg.reply(obj[content]);
 		}
+
+	else if (!isNaN(content[0])) {
+				let reply = roll.parse(content);
+	
+				msg.reply(reply).catch((error) => {
+					console.log(error);
+					msg.reply(
+						"The bot had an error, which has been logged.\n*" +
+							error.message +
+							"*"
+					);
+				});
+			}
 	}
 });
 
